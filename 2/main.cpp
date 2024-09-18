b31c1b0f6fdaa9c06c9358913f009e4bc5f40143
Kikito <kylian.jozefiak@etu.unistra.fr>
2024-09-18 15:43:12 +0200

    q2

--- 2/main.cpp
+++ 2/main.cpp
@@ -2,31 +2,53 @@
 #include <fstream>
 #include <regex>
 #include <chrono>
+#include <vector>
+#include <string>
 
-int main () {
+bool is_in_vector(const std::vector<std::string> &words, const std::string &word)
+{
+	for (const std::string &w : words)
+	{
+		if (w == word)
+			return true;
+	}
+	return false;
+}
+
+int main()
+{
 	using namespace std;
 	using namespace std::chrono;
 
-	ifstream input = ifstream("/tmp/WarAndPeace.txt");
+	ifstream input = ifstream("WarAndPeace.txt");
 
 	auto start = steady_clock::now();
 	cout << "Parsing War and Peace" << endl;
+
+	// vecteur dans lequel on metra les mots rencontrés
+	vector<string> words;
 
 	size_t nombre_lu = 0;
 	// prochain mot lu
 	string word;
 	// une regex qui reconnait les caractères anormaux (négation des lettres)
-	regex re( R"([^a-zA-Z])");
-	while (input >> word) {
+	regex re(R"([^a-zA-Z])");
+	while (input >> word)
+	{
 		// élimine la ponctuation et les caractères spéciaux
-		word = regex_replace ( word, re, "");
+		word = regex_replace(word, re, "");
 		// passe en lowercase
-		transform(word.begin(),word.end(),word.begin(),::tolower);
-
+		transform(word.begin(), word.end(), word.begin(), ::tolower);
 		// word est maintenant "tout propre"
+
+		if (is_in_vector(words, word))
+			continue;
+
+		words.push_back(word);
+
 		if (nombre_lu % 100 == 0)
 			// on affiche un mot "propre" sur 100
-			cout << nombre_lu << ": "<< word << endl;
+			cout << nombre_lu << ": " << word << endl;
 		nombre_lu++;
 	}
 	input.close();
@@ -34,13 +56,11 @@ int main () {
 	cout << "Finished Parsing War and Peace" << endl;
 
 	auto end = steady_clock::now();
-    cout << "Parsing took "
-              << duration_cast<milliseconds>(end - start).count()
-              << "ms.\n";
+	cout << "Parsing took "
+		 << duration_cast<milliseconds>(end - start).count()
+		 << "ms.\n";
 
-    cout << "Found a total of " << nombre_lu << " words." << endl;
+	cout << "Found a total of " << nombre_lu << " words." << endl;
 
-    return 0;
+	return 0;
 }
-
-
